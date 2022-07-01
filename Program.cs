f4f7004ae3cec45cc3bf616a73a8c01aa19900fd
kelsey <enktal@gmail.com>
2022-06-30 17:30:02 -0700

    write demo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IrcDotNet
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: efnet <username>");
+                Console.WriteLine("Demo program (c) Kelsey 2022");
+                return;
+            }
+            var server = new System.Uri("irc.efnet.org");
+            var username = args[0];
+            Console.WriteLine("Connecting to {0} as {1}", server, username);
+            using (var client = new IrcDotNet.StandardIrcClient())
+            {
+                client.Connect(server, new IrcUserRegistrationInfo { NickName = username });
+            }
+        }
+    }
+}
